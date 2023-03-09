549bdc0c7c4d2f6aaa9845363770659a0392a426
Cristian <20001666@alumnos.utleon.edu.mx>
2023-03-08 19:38:46 -0600

    Trabajo realizado 08/03/23

--- main.py
+++ main.py
@@ -18,6 +18,44 @@ def index():
         db.session.commit()
     return render_template('index.html', form = create_form)
 
+@app.route('/abcompleto', methods=['GET', 'POST'])
+def abc():
+    create_form = forms.UserForm(request.form)
+    #select * from alumnos
+    alumnos = Alumno.query.all()
+    return render_template('abcompleto.html', form = create_form, alumnos=alumnos)   
+
+@app.route('/modificar', methods=['GET', 'POST'])
+def modificar_alumno():
+    create_form = forms.UserForm(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        # SELECT * FROM alumnos WHERE id = id
+        alumno = db.session.query(Alumno).filter(Alumno.id == id).first()
+        create_form.id.data = alumno.id     
+        create_form.nombre.data =  alumno.nombre
+        create_form.apaterno.data = alumno.apaterno
+        create_form.email.data = alumno.email
+
+    if request.method == 'POST':
+        id = create_form.id.data
+        alumno = db.session.query(Alumno).filter(Alumno.id==id).first()
+        alumno.nombre = request.form.get('nombre')
+        alumno.apaterno = request.form.get('apaterno')
+        alumno.email =  request.form.get('email')
+        db.session.add(alumno)
+        db.session.commit()
+        return redirect('/abcompleto')
+    return render_template('modificar.html', form = create_form)
+
+@app.route('/eliminar', methods=['GET', 'POST'])
+def eliminar_alumno():
+    id = request.args.get('id')
+    alumno = db.session.query(Alumno).filter(Alumno.id==id).first()
+    db.session.delete(alumno)
+    db.session.commit()  
+    return redirect('/abcompleto')
+
 if __name__ == '__main__':
     csrf.init_app(app)
     db.init_app(app)
